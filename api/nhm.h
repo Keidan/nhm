d8fb9b2c257bf04c5443aa0b013537b522d23f43
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2015-11-10 08:18:12 +0100

    DEV: Update comments + remove macro

--- api/nhm.h
+++ api/nhm.h
@@ -74,35 +74,62 @@ struct nhm_s {
     bytes[offset] = (value & 0xFF);		\
   })
 
-#define nhm_is_same(n1, n2) (memcmp((n1)->hw, (n2)->hw, NHM_LENGTH) == 0 \
-			     && (n1)->ip4 == (n2)->ip4			\
-			     && memcmp((n1)->port, (n2)->port,		\
-				       2*sizeof(unsigned short)) == 0	\
-			     && (n1)->eth_proto == (n2)->eth_proto	\
-			     && (n1)->ip_proto == (n2)->ip_proto)
-
 /* 
- * Adds a new entry
+ * Adding a new entry pointed by the IOCTL argument.
+ * @code
+ * struct nhm_s n;
+ * memset(&n, 0, NHM_LENGTH);
+ * n.ip4 = nhm_to_ip(192, 168, 0, 1);
+ * n.dir = NHM_DIR_BOTH;
+ * n.nf_type = NHM_NF_TYPE_DROP;
+ * // After this call all packets from the ip 192.168.0.1 or to the ip 192.168.0.1 will be ignored.
+ * ioctl(fd, NHM_IOCTL_ADD, &n);
+ * @endcode
  */
 #define NHM_IOCTL_ADD    _IOW(NHM_MAJOR_NUMBER, 0, struct nhm_s *)
 /* 
- * Removes an entry
+ * Removing an existing entry pointed by the IOCTL argument.
+ * @code
+ * struct nhm_s n;
+ * memset(&n, 0, NHM_LENGTH);
+ * n.ip4 = nhm_to_ip(192, 168, 0, 1);
+ * // After this call all packets from the ip 192.168.0.1 or to the ip 192.168.0.1 will be accepted.
+ * ioctl(fd, NHM_IOCTL_DEL, &n);
+ * @endcode
  */
 #define NHM_IOCTL_DEL    _IOW(NHM_MAJOR_NUMBER, 1, struct nhm_s *)
 /* 
- * Removes all entries.
+ * Removing all entries.
+ * @code
+ * // After this call all entries will be removed.
+ * ioctl(fd, NHM_IOCTL_CLEAR, NULL);
+ * @endcode
  */
 #define NHM_IOCTL_CLEAR  _IOW(NHM_MAJOR_NUMBER, 2, void *)
 /* 
- * Clear the read index.
+ * Changes the read index and places it to the head of the list.
+ * @code
+ * // After this call the read index will be reseted to the head of the list.
+ * ioctl(fd, NHM_IOCTL_ZERO, NULL);
+ * @endcode
  */
 #define NHM_IOCTL_ZERO   _IOW(NHM_MAJOR_NUMBER, 3, void *)
 /*
- * Get the read length
+ * Returns the number of items that the read function can return.
+ * @code
+ * int length;
+ * // After this call the length field contain the number of inserted elements.
+ * ioctl(fd, NHM_IOCTL_LENGTH, &length);
+ * @endcode
  */
 #define NHM_IOCTL_LENGTH _IOR(NHM_MAJOR_NUMBER, 4, int *)
 /*
- * Change the default netfilter type
+ * Changes the globale netfilter type to be apply by default.
+ * @code
+ * nhm_nf_type_te type = NHM_NF_TYPE_DROP;
+ * // After this call all the packets will be dropped by default.
+ * ioctl(fd, NHM_IOCTL_NF_TYPE, NULL);
+ * @endcode
  */
 #define NHM_IOCTL_NF_TYPE _IOW(NHM_MAJOR_NUMBER, 5, nhm_nf_type_te *)
 
