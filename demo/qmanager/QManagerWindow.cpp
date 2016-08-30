e260b04da56f799e24ab0933ac43952550b5718a
Keidan <kuso.fr@gmail.com>
2016-08-30 15:38:40 +0200

    fix indent

--- demo/qmanager/QManagerWindow.cpp
+++ demo/qmanager/QManagerWindow.cpp
@@ -1,27 +1,27 @@
 /**
- *******************************************************************************
- * @file QManagerWindow.hpp
- * @author Keidan
- * @date 30/08/2016
- * @par Project nhm->qmanager
- *
- * @par Copyright 2016 Keidan, all right reserved
- *
- *      This software is distributed in the hope that it will be useful, but
- *      WITHOUT ANY WARRANTY.
- *
- *      License summary : You can modify and redistribute the sources code and
- *      binaries. You can send me the bug-fix
- *
- *      Term of the license in in the file license.txt.
- *    _____
- *   /     \ _____    ____ _____     ____   ___________
- *  /  \ /  \\__  \  /    \\__  \   / ___\_/ __ \_  __  \
- * /    Y    \/ __ \|   |  \/ __ \_/ /_/  >  ___/|  | \/
- * \____|__  (____  /___|  (____  /\___  / \___  >__|
- *         \/     \/     \/     \//_____/      \/
- *******************************************************************************
- */
+*******************************************************************************
+* @file QManagerWindow.hpp
+* @author Keidan
+* @date 30/08/2016
+* @par Project nhm->qmanager
+*
+* @par Copyright 2016 Keidan, all right reserved
+*
+*      This software is distributed in the hope that it will be useful, but
+*      WITHOUT ANY WARRANTY.
+*
+*      License summary : You can modify and redistribute the sources code and
+*      binaries. You can send me the bug-fix
+*
+*      Term of the license in in the file license.txt.
+*    _____
+*   /     \ _____    ____ _____     ____   ___________
+*  /  \ /  \\__  \  /    \\__  \   / ___\_/ __ \_  __  \
+* /    Y    \/ __ \|   |  \/ __ \_/ /_/  >  ___/|  | \/
+* \____|__  (____  /___|  (____  /\___  / \___  >__|
+*         \/     \/     \/     \//_____/      \/
+*******************************************************************************
+*/
 #include "QManagerWindow.hpp"
 #include "ui_QManagerWindow.h"
 #include <QString>
